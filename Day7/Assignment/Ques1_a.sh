e7c9157ba22a122eecaa58b485ad0a3c2dc71efe
rajpal229 <rajpalrawat229@gmail.com>
2022-11-25 11:55:22 +0530

    Updated

---
+++ Day7/Assignment/Ques1_a.sh
@@ -0,0 +1,22 @@
+number=0;
+randomNumber[((number++))]=$((random_1=$((((RANDOM%89+10))))));
+randomNumber[((number++))]=$((random_2=$((((RANDOM%89+10))))));
+randomNumber[((number++))]=$((random_3=$((((RANDOM%89+10))))));
+randomNumber[((number++))]=$((random_4=$((((RANDOM%89+10))))));
+randomNumber[((number++))]=$((random_5=$((((RANDOM%89+10))))));
+randomNumber[((number++))]=$((random_6=$((((RANDOM%89+10))))));
+randomNumber[((number++))]=$((random_7=$((((RANDOM%89+10))))));
+randomNumber[((number++))]=$((random_8=$((((RANDOM%89+10))))));
+randomNumber[((number++))]=$((random_9=$((((RANDOM%89+10))))));
+randomNumber[((number++))]=$((random_10=$((((RANDOM%89+10))))));
+
+echo ${randomNumber[0]}
+echo ${randomNumber[1]}
+echo ${randomNumber[2]}
+echo ${randomNumber[3]}
+echo ${randomNumber[4]}
+echo ${randomNumber[5]}
+echo ${randomNumber[6]}
+echo ${randomNumber[7]}
+echo ${randomNumber[8]}
+echo ${randomNumber[9]}
